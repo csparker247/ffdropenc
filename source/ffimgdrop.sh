880d208425e5f568a19724c8cc951cd16f6c757d
Seth Parker <csparker247@gmail.com>
2013-09-12 19:12:32 -0400

    #BUGGY Setup for ability to accept video and image sequences.

--- source/ffimgdrop.sh
+++ source/ffimgdrop.sh
@@ -16,12 +16,13 @@ fi
 	preset_script=()
 	preset_name=()
 	preset_count="0"
+	has_sequences="n"
 	
 	for i in presets/*.sh; do
 		preset_type=$(sed -n /optname/p "$i" | sed 's/# optname //')
-		if [ ! $preset_type = "" ]; then
+		if [[ "$preset_type" != "" ]]; then
 			preset_script+="$(basename "$i")"
-			preset_name+="$preset_type"
+			preset_name+=$preset_type
 		fi
 	done
 	# Count number of presets
@@ -30,88 +31,111 @@ fi
 # FPS Setup
 	fps_options=("24" "25" "29.97" "30" "60")
 	fps_count="${#fps_options[@]}"
-	
+
+
 # Collect list of approved file extensions
+filters=`cat filters.db`
 sequence_exts=`cat seqexts.db`
-exts=`cat exts.db`
+mov_exts=`cat movexts.db`
 
 # Setup qtfaststart
 qtfaststart="bin/qtfaststart/qtfaststart"
 
-
 ## Build list of files to encode, using only files from approved extensions list.
-sequencelist=()
+argument_files=()
 filelist=()
-echo Building file list.
-while [ ! -z "$1" ]; do
-	if [[ "$1" =~ .*\.($sequence_exts) ]]; then
+
+echo "Building file list."
+for argument in $@; do
+	OLDIFS=$IFS
+	IFS=$'\n'
+	argument_files+=($(find "$argument" -type f | grep -e ".*/.*\.\($filters)"))
+	IFS=$OLDIFS
+done
+
+for thisfile in $argument_files; do
+	echo $thisfile
+	if [[ "$thisfile" =~ .*\.($sequence_exts) ]]; then
+		has_sequences="y"
 		inlist="n"
-		tempdir=$(dirname "$1")
-		tempext=$(basename "$1" | sed 's/.*\.\(.*\)/\1/')
-		tempname=$(basename "$1" | sed 's/\(.*\)\..*/\1/')
+		tempdir=$(dirname "$thisfile")
+		tempext=$(basename "$thisfile" | sed 's/.*\.\(.*\)/\1/')
+		tempname=$(basename "$thisfile" | sed 's/\(.*\)\..*/\1/')
 		collection=$(echo "$tempname" | sed 's/[0-9]*$//')
 		chartemp=$(echo "$tempname" | grep -o -m 1 -e '[0-9]*$')
+		
 		if [[ "$chartemp" != [0-9]* ]]; then
-			shift
 			continue
 		fi
- 		charcount=`printf "%02d" ${#chartemp}`
-		new_path="${tempdir}/${collection}%${charcount}d.${tempext}"
 		
-		for e in "${filelist[@]}"; do
+		charcount=`printf "%02d" ${#chartemp}`
+		new_path="${tempdir}/${collection}%${charcount}d.${tempext}"
+	
+		for e in $filelist; do
 			if [[ "$e" == "$new_path" ]]; then
 				inlist="y"
 				break
 			fi
 		done
-		
+	
 		if [[ "$inlist" == "n" ]]; then
+			OLDIFS=$IFS
+			IFS=$'\n'
 			filelist+=("$new_path")
+			IFS=$OLDIFS
 		fi
-	#else
-	#OLDIFS=$IFS
-	#IFS=$'\n'
-	#filelist+=($(find "$1" -type f | grep -e ".*/.*\.\($exts)"))
-	#IFS=$OLDIFS
+		
+	elif [[ "$thisfile" =~ .*\.($mov_exts) ]]; then
+		OLDIFS=$IFS
+		IFS=$'\n'
+		filelist+=("$thisfile")
+		IFS=$OLDIFS
 	fi
-	shift	
 done
 
-echo ${filelist[@]}
+echo "I made it!"
+unset argument_files
 
 # Setup Platypus counter
 count=0
 args=${#filelist[@]}
 
+if [[ "$args" == "0" ]]; then
+	echo "No supported file types in batch list."
+	exit 0
+fi
+
 # Ask for encoding settings
+
 enc_sets=(`bin/cocoaDialog.app/Contents/MacOS/cocoaDialog standard-dropdown --title "ffimgdrop" --text "Select output type." --height 150 --items $preset_name`)
-select_fps=(`bin/cocoaDialog.app/Contents/MacOS/cocoaDialog standard-dropdown --title "ffimgdrop" --text "Select output fps:" --height 150 --items $fps_options`)
+if [[ "$has_sequences" == "y" ]]; then
+	select_fps=(`bin/cocoaDialog.app/Contents/MacOS/cocoaDialog standard-dropdown --title "ffimgdrop" --text "Select output fps." --height 150 --items $fps_options`)
+	# Convert selected value to account for lack of 0 index
+		fps_type="$(echo "scale=1; ${select_fps[2]}+1" | bc)"
 
-# Convert selected value to account for lack of 0 index
-fps_type="$(echo "scale=1; ${select_fps[2]}+1" | bc)"
-
-# Figure fps from selected option
-if [ "${select_fps[1]}" = "2" ]; then
-		echo "Encoding cancelled!"
-		exit 1
-	else
-		for j in {1..$fps_count}; do
-			if [ $fps_type = $j ]; then
-				enc_fps="$fps_options[$fps_type]"
-			fi
-		done
+	# Figure fps from selected option
+	if [[ "${select_fps[1]}" == "2" ]]; then
+			echo "Encoding cancelled!"
+			exit 1
+		else
+			for j in {1..$fps_count}; do
+				if [[ $fps_type == $j ]]; then
+					enc_fps="$fps_options[$fps_type]"
+				fi
+			done
+	fi
 fi
 
 # Convert selected value to account for lack of 0 index
 enc_type="$(echo "scale=1; ${enc_sets[2]}+1" | bc)"
 
 # Check for start approval and call the appropriate encoder
-if [ "${enc_sets[1]}" = "2" ]; then
+if [[ "${enc_sets[1]}" == "2" ]]; then
 		echo "Encoding cancelled!"
 		exit 1
 	else
 		for j in {1..$preset_count}; do
-			if [ $enc_type = $j ]; then
+			if [[ $enc_type == $j ]]; then
 				. presets/$preset_script[$enc_type] 
 			fi
 		done	
--- source/presets/h264-noscale.sh
+++ source/presets/h264-noscale.sh
@@ -5,24 +5,32 @@
 # Encode each file
 for (( i=1; i<=${args}; i++ ));
 	do
+		SEQ_OPTS=""
 		# Remove the extension and make filenames for logs and output.
 			INFILE="$(basename "${filelist[$i]}")"
-			SETNAME="$(echo "${filelist[$i]}" | sed 's/%[0-9]*d\..*//')"
-			if [[ $SETNAME != *(_|-) ]]; then
-				ERRLOG="$SETNAME"_H264.log
-				OUTFILE="$SETNAME"_H264.mp4
+			if [[ "$INFILE" =~ .*\.($sequence_exts) ]]; then
+				SEQ_OPTS="-f image2 -r $enc_fps"
+				SETNAME="$(echo "${filelist[$i]}" | sed 's/%[0-9]*d\..*//')"
+				if [[ $SETNAME != *(_|-|" ") ]]; then
+					ERRLOG="$SETNAME"_H264.log
+					OUTFILE="$SETNAME"_H264.mp4
+				else
+					ERRLOG="$SETNAME"H264.log
+					OUTFILE="$SETNAME"H264.mp4
+				fi
 			else
-				ERRLOG="$SETNAME"H264.log
-				OUTFILE="$SETNAME"H264.mp4
+			RAWNAME="$(echo "${filelist[$i]}" | sed 's/\(.*\)\..*/\1/')"
+			ERRLOG="$RAWNAME"_H264.log
+			OUTFILE="$RAWNAME"_H264.mp4
 			fi
-			
+
 		# Type of encode: 1 = single pass, 2 = two-pass, 3 = three-pass/two-pass+audio, etc.
 			NUM_PASSES="1"
 						
 		# Video pass
 			echo "Encoding H.264 Generic (No Scale) Version of $INFILE"
 			ENCODER="FFMPEG"
-			ffmpeg -f image2 -r "$enc_fps" -i "${filelist[$i]}" -c:v libx264 -b:v 10M -maxrate 10M -bufsize 3M -pix_fmt yuv420p -c:a libfdk_aac -profile:a aac_low -b:a 320k -ar 48k -y "$OUTFILE" \
+			ffmpeg $(echo $SEQ_OPTS) -i "${filelist[$i]}" -c:v libx264 -b:v 10M -maxrate 10M -bufsize 3M -pix_fmt yuv420p -c:a libfdk_aac -profile:a aac_low -b:a 320k -ar 48k -y "$OUTFILE" \
 			2>&1 | awk '1;{fflush()}' RS='\r\n'>"$ERRLOG" &
 			
 		# Track encoding progress	
@@ -37,6 +45,6 @@ for (( i=1; i<=${args}; i++ ));
 			PROG=$(echo "scale=3; ($count/$args)*100.0" | bc)
 			echo PROGRESS:"$PROG"
 		# Cleanup
-			rm "$ERRLOG"			
+			#rm "$ERRLOG"			
 	done
 exit 0
