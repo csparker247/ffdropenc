6e4a43b0e78f5740df3ed7123f6d455a65517298
Seth Parker <csparker247@gmail.com>
2014-01-15 17:56:38 -0500

    New MKV with Audio Pass-thru preset. Utilizes experimental adaptive antialiasing.

---
+++ source/presets/mkv-audiopass.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# optname MKV (H.264, Audio Pass-thru)
+
+# Encode each file
+for (( i=1; i<=${args}; i++ )); do
+		SEQ_OPTS=""
+		index=$(expr $i - 1)
+		# Remove the extension and make filenames for logs and output.
+			INFILE="$(basename "${filelist[$index]}")"
+			if [[ "$useNewOutput" == "1" ]]; then
+				OUTPATH="$newOutputPath"
+			else
+				OUTPATH="$(dirname "${filelist[$index]}")"
+			fi
+			if [[ "$INFILE" =~ .*\.($sequence_exts) ]]; then
+				SEQ_OPTS="-f image2 -r $enc_fps"
+				SETNAME="$(echo "$INFILE" | sed 's/%[0-9]*d\..*//')"
+				if [[ "$SETNAME" =~ .*(\_|\-|" ") ]]; then
+					ERRLOG="${OUTPATH}/${SETNAME}"H264.log
+					OUTFILE="${OUTPATH}/${SETNAME}"H264.mkv
+				else
+					ERRLOG="${OUTPATH}/${SETNAME}"_H264.log
+					OUTFILE="${OUTPATH}/${SETNAME}"_H264.mkv
+				fi
+			else
+				RAWNAME="$(echo "$INFILE" | sed 's/\(.*\)\..*/\1/')"
+				ERRLOG="${OUTPATH}/${RAWNAME}"_H264.log
+				OUTFILE="${OUTPATH}/${RAWNAME}"_H264.mkv
+			fi
+
+		# Type of encode: 1 = single pass, 2 = two-pass, 3 = three-pass/two-pass+audio, etc.
+			NUM_PASSES="1"
+						
+		# Video pass
+			echo "Encoding MKV (H.264, Audio Pass-thru) Version of $INFILE"
+			ENCODER="FFMPEG"
+			ffmpeg $(echo $SEQ_OPTS) -i "${filelist[$index]}" -map 0 -c copy -c:v libx264 -crf 16 -maxrate 45M -bufsize 40M -pix_fmt yuv420p -vf "scale=iw*sar:ih, idet, w3fdif=complex:interlaced" -y "$OUTFILE" \
+			2>&1 | awk '1;{fflush()}' RS='\r\n'>"$ERRLOG" &
+			
+		# Track encoding progress	
+			. progress.sh
+				
+		# Update progress
+			count=$(echo "scale=3; ($count+1)" | bc)
+			PROG=$(echo "scale=3; ($count/$args)*100.0" | bc)
+			echo PROGRESS:"$PROG"
+		# Cleanup
+			rm "$ERRLOG"			
+	done
+exit 0
