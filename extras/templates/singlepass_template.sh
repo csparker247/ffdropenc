62e22350ac3c693d0176115a6b1db11d572a72f5
Seth Parker <csparker247@gmail.com>
2014-01-22 15:33:01 -0500

    Rearrange code and first working version of auto-build system.

---
+++ extras/templates/singlepass_template.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# optname [SINGLE-PASS TEMPLATE]
+
+# Encode each file
+for (( i=1; i<=${args}; i++ )); do
+		SEQ_OPTS=""
+		index=$(expr $i - 1)
+		# Remove the extension and make filenames for logs and output.
+			INFILE="$(basename "${filelist[$index]}")"
+			if [[ "$INFILE" =~ .*\.($sequence_exts) ]]; then
+				SEQ_OPTS="-f image2 -r $enc_fps"
+				SETNAME="$(echo "${filelist[$index]}" | sed 's/%[0-9]*d\..*//')"
+				if [[ "$SETNAME" =~ .*(\_|\-|" ") ]]; then
+					ERRLOG="$SETNAME"SINGLEPASSTEMPLATE.log
+					OUTFILE="$SETNAME"SINGLEPASSTEMPLATE.mp4
+				else
+					ERRLOG="$SETNAME"_SINGLEPASSTEMPLATE.log
+					OUTFILE="$SETNAME"_SINGLEPASSTEMPLATE.mp4
+				fi
+			else
+			RAWNAME="$(echo "${filelist[$index]}" | sed 's/\(.*\)\..*/\1/')"
+			ERRLOG="$RAWNAME"_SINGLEPASSTEMPLATE.log
+			OUTFILE="$RAWNAME"_SINGLEPASSTEMPLATE.mp4
+			fi
+			
+		# Type of encode: 1 = single pass, 2 = two-pass, 3 = three-pass/two-pass+audio, etc.
+			NUM_PASSES="1"
+						
+		# Video pass
+			echo "Encoding [SINGLE-PASS TEMPLATE] Version of $INFILE"
+			ENCODER="FFMPEG"
+			ffmpeg $(echo $SEQ_OPTS) -i "${filelist[$index]}" -c:v libx264 -b:v 2500K -pix_fmt yuv420p -profile:v baseline -vf "scale=iw*sar:ih, scale='if(gt(iw,ih),min(720,iw),-1)':'if(gt(iw,ih),-1,min(480,ih))'" -c:a libfdk_aac -b:a 192k -ar 48k -y "$OUTFILE" \
+			2>&1 | awk '1;{fflush()}' RS='\r\n'>"$ERRLOG" &
+			
+		# Track encoding progress	
+			. progress.sh
+		
+		# Move the faststart atom
+		echo Moving moov atom.	
+		"$qtfaststart" "$OUTFILE"
+				
+		# Update progress
+			count=$(echo "scale=3; ($count+1)" | bc)
+			PROG=$(echo "scale=3; ($count/$args)*100.0" | bc)
+			echo PROGRESS:"$PROG"
+		# Cleanup
+			rm "$ERRLOG"			
+	done
+exit 0
