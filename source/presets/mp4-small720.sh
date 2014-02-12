67432950f1649e8f74a27fd63ca7777abef9ae47
Seth Parker <csparker247@gmail.com>
2014-02-12 14:08:01 -0500

    Finalizing Small MP4 presets

---
+++ source/presets/mp4-small720.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# optname Small MP4 720p HD (Very Slow Encode)
+
+# Encode each file
+for (( i=1; i<=${args}; i++ )); do
+		SEQ_OPTS=""
+		index=$(expr $i - 1)
+		# Remove the extension and make filenames for logs and output.
+			INFILE="$(basename "${filelist[$index]}")"
+			if [[ "$useNewOutput" == "1" ]]; then
+				OUTPATH="$newOutputPath"
+			else
+				OUTPATH="$(dirname "${filelist[$index]}")"
+			fi
+			if [[ "$INFILE" =~ .*\.($sequence_exts) ]]; then
+				SEQ_OPTS="-f image2 -r $enc_fps"
+				SETNAME="$(echo "$INFILE" | sed 's/%[0-9]*d\..*//')"
+				if [[ "$SETNAME" =~ .*(\_|\-|" ") ]]; then
+					ERRLOG="${OUTPATH}/${SETNAME}"Small720.log
+					OUTFILE="${OUTPATH}/${SETNAME}"Small720.mp4
+				else
+					ERRLOG="${OUTPATH}/${SETNAME}"_Small720.log
+					OUTFILE="${OUTPATH}/${SETNAME}"_Small720.mp4
+				fi
+			else
+				RAWNAME="$(echo "$INFILE" | sed 's/\(.*\)\..*/\1/')"
+				ERRLOG="${OUTPATH}/${RAWNAME}"_Small720.log
+				OUTFILE="${OUTPATH}/${RAWNAME}"_Small720.mp4
+			fi
+			
+		# Type of encode: 1 = single pass, 2 = two-pass, 3 = three-pass/two-pass+audio, etc.
+			NUM_PASSES="1"
+						
+		# Video pass
+			echo "Encoding Small MP4 720p HD Version of $INFILE"
+			ENCODER="FFMPEG"
+			ffmpeg $(echo $SEQ_OPTS) -i "${filelist[$index]}" \
+			-c:v libx264 -crf 18 -maxrate 3M -bufsize 6M -profile:v high -level 42 -pix_fmt yuv420p -preset veryslow -vf "scale=iw*sar:ih, scale='if(gt(iw,ih),min(1280,iw),-1)':'if(gt(iw,ih),-1,min(720,ih))'" -movflags faststart \
+			-c:a libfdk_aac -b:a 192k \
+			-y "$OUTFILE" \
+			2>&1 | awk '1;{fflush()}' RS='\r\n'>"$ERRLOG" &
+			
+		# Track encoding progress	
+			. progress.sh
+				
+		# Update progress
+			count=$(echo "scale=3; ($count+1)" | bc)
+			PROG=$(echo "scale=3; ($count/$args)*100.0" | bc)
+			echo PROGRESS:"$PROG"
+		# Cleanup
+			rm "$ERRLOG"			
+	done
+exit 0
