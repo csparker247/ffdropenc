89a6696fec743c86c9bcd5f9ccbdab69eec376f0
csparker247 <csparker247@gmail.com>
2014-09-22 00:54:26 -0400

    Link with libconfig and first example of preset config parsing.

--- src/ffdropenc.cxx
+++ src/ffdropenc.cxx
@@ -1,12 +1,14 @@
 // ffdropenc
 #include <iostream>
-#include <iomanip>
+#include <string>
+#include <vector>
+
+#include <libconfig.h++>
 
 #include "ffdropenc.h"
 
-#include "cereal/cereal.hpp"
-
 using namespace std;
+using namespace libconfig;
 
 int main (int argc, char* argv[]) {
   cout << endl;
@@ -16,8 +18,35 @@ int main (int argc, char* argv[]) {
   cout << endl;
   
 // Parse command line options
-  string preset = argv[1];
-  vector<string> inputfiles(argv + 2, argv + argc);
+  string preset;
+  vector<string> inputlist;
+
+  preset = argv[1];
+  for (int i = 2; i < argc; i++) {
+    inputlist.push_back(argv[i]);  
+  };
+
+  Config cfg;
+
+  // Read the file. If there is an error, report it and exit.
+  try
+  {
+    cfg.readFile("../presets/appletv.cfg");
+  }
+  catch(const FileIOException &fioex)
+  {
+    std::cerr << "I/O error while reading file." << std::endl;
+    return 1;
+  }
+  catch(const ParseException &pex)
+  {
+    std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+              << " - " << pex.getError() << std::endl;
+    return 1;
+  }
+
+  string name = cfg.lookup("preset.listname");
+  cout << "Preset name: " << name << endl << endl;
 
   return 0;
 }
