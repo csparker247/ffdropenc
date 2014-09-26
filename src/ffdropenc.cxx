0c202372f246f30ecac81fc1018a85e3031cd29f
csparker247 <csparker247@gmail.com>
2014-09-25 20:35:46 -0400

    Generate list of presets based on cfg files in presets directory. User can select which preset to use.

--- src/ffdropenc.cxx
+++ src/ffdropenc.cxx
@@ -18,35 +18,41 @@ int main (int argc, char* argv[]) {
   cout << endl;
   
 // Parse command line options
-  string preset;
+  int preset;
   vector<string> inputlist;
 
-  preset = argv[1];
-  for (int i = 2; i < argc; i++) {
+  preset = stoi(argv[1]);
+  for (int i = 2; i < argc; ++i) {
     inputlist.push_back(argv[i]);  
   };
 
+// Load all the preset files
+  vector<string> presetList;
+  vector<string> presetFiles;
+  loadPresets(presetList, presetFiles);
+  
+  
+
+// Sanity check: output the loaded presets and the file list
+  cout << "Loaded the following presets:" << endl;
+  for (int i = 0; i < presetList.size(); ++i) {
+    cout << "  " << i << ": " << presetList[i] << endl;
+  }
+  cout << endl;
+
+  cout << "File list:" << endl;
+  for (int i = 0; i < inputlist.size(); ++i) {
+    cout << "  " << i << ": " << inputlist[i] << endl;
+  }
+  cout << endl;
+
+// Load the user's preset
   Config cfg;
-
-  // Read the file. If there is an error, report it and exit.
-  try
-  {
-    cfg.readFile("../presets/appletv.cfg");
-  }
-  catch(const FileIOException &fioex)
-  {
-    std::cerr << "I/O error while reading file." << std::endl;
-    return 1;
-  }
-  catch(const ParseException &pex)
-  {
-    std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
-              << " - " << pex.getError() << std::endl;
-    return 1;
-  }
-
+  string presetPath;
+  presetPath = presetDir + presetFiles[preset];
+  cfg.readFile(presetPath.c_str());
   string name = cfg.lookup("preset.listname");
-  cout << "Preset name: " << name << endl << endl;
+  cout << "Selected preset: " << name << endl << endl;
 
   return 0;
 }
--- src/ffdropenc.h
+++ src/ffdropenc.h
@@ -3,4 +3,58 @@
 
 #define VERSION "2.0"
 
+#include <dirent.h>
+#include <unistd.h>
+
+std::string presetDir = "../presets/";
+std::string cfg_extension = "cfg";
+
+// recursive search algorithm
+void search(std::string directory, std::string extension, std::vector<std::string>& results) {
+  DIR* dir_point = opendir(directory.c_str());
+  dirent* entry = readdir(dir_point);
+  while (entry){                  // if !entry then end of directory
+    if (entry->d_type == DT_DIR){       // if entry is a directory
+      std::string fname = entry->d_name;
+      if (fname != "." && fname != "..")
+        search(entry->d_name, extension, results); // search through it
+    }
+    else if (entry->d_type == DT_REG){    // if entry is a regular file
+      std::string fname = entry->d_name;  // filename
+      if (fname.find(extension, (fname.length() - extension.length())) != std::string::npos)
+        results.push_back(fname);   // add filename to results vector
+    }
+    entry = readdir(dir_point);
+  }
+  return;
+}
+
+void loadPresets(std::vector<std::string>& presetList, std::vector<std::string>& presetFiles) {
+  std::string presetPath;
+  libconfig::Config cfg;
+  
+  search(presetDir, cfg_extension, presetFiles);
+
+  if (presetFiles.size()){
+    for (int i = 0; i < presetFiles.size(); ++i) {
+      try {
+        presetPath = presetDir + presetFiles[i];
+        cfg.readFile(presetPath.c_str());
+        std::string presetName = cfg.lookup("preset.listname");
+        presetList.push_back(presetName);
+      }
+      catch (const libconfig::FileIOException &fioex) {
+        std::cerr << "I/O error while reading file: " << presetFiles[i] << std::endl;
+      }
+      catch (const libconfig::ParseException &pex) {
+        std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+                  << " - " << pex.getError() << std::endl;
+      }
+    }
+  }
+  else{
+    std::cerr << "Error: No preset files were found." << std::endl;
+  }
+}
+
 #endif
