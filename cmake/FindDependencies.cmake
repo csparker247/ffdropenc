f6e74400dfcdcfce1d71b845e8f03bc8c23a6e20
Seth Parker <csparker247@gmail.com>
2019-08-08 12:22:23 -0400

    Move Qt5 deployment stuff into an cmake module.

--- cmake/FindDependencies.cmake
+++ cmake/FindDependencies.cmake
@@ -2,17 +2,8 @@ include(FetchContent)
 
 # QT
 find_package(Qt5 5.7 QUIET REQUIRED COMPONENTS Core Widgets Gui)
-
-if((APPLE OR WIN32) AND NOT DEPLOYQT_EXECUTABLE)
-    get_target_property(_qmake_bin Qt5::qmake IMPORTED_LOCATION)
-    get_filename_component(_qt_bin_dir "${_qmake_bin}" DIRECTORY)
-    find_program(DEPLOYQT_EXECUTABLE NAMES windeployqt macdeployqt HINTS "${_qt_bin_dir}")
-    if(DEPLOYQT_EXECUTABLE)
-        message(STATUS "Found Qt deploy tool: ${DEPLOYQT_EXECUTABLE}")
-    else()
-        message(SEND_ERROR "Qt deploy tool not found. Modify your CMAKE_PREFIX_PATH "
-                "or manually set DEPLOYQT_EXECUTABLE to the location of the executable.")
-    endif()
+if(APPLE OR WIN32)
+    find_package(DeployQt5 REQUIRED)
 endif()
 
 # JSON
